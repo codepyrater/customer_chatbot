7ddbd0e7ed1564105127be21f23fdd26d4ddb225
codepyrater <punna1s@cmich.edu>
2024-07-14 14:22:56 -0400

    added bar

--- src/components/chatbot_window/chatbot_window.tsx
+++ src/components/chatbot_window/chatbot_window.tsx
@@ -1,11 +1,26 @@
  
-import {   Container,   } from '@mui/material';
+import {   AppBar, Avatar, Box, Container, Paper, Toolbar, Typography,   } from '@mui/material';
 import InputField from '../inputfield/inputfield';
  
 const Chatbot_Window = () => {
   return (
-   <Container>
-<InputField/>
+   <Container maxWidth="md" sx={{ padding: 0, display: 'flex', flexDirection: 'column', height: '100vh' }}>
+          <AppBar position="static" color="primary">
+        <Toolbar>
+           
+          <Avatar alt="Bot" src="assests\image.png" />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: 2 }}>
+            Stormy Bot
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Paper elevation={3} sx={{ padding: 2,  flexGrow: 1, display: 'flex', flexDirection: 'column', height: 'calc(100vh - 64px)' }}>
+        </Paper>
+      
+      <Box>
+      <InputField/>
+      </Box>
+
    </Container>
   )
 }
